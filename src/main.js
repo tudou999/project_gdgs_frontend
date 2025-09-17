d222a6c606057e395b369b91c7cab27f62e516ac
Chen Zhuofan <148603759+tudou999@users.noreply.github.com>
2025-09-17 16:12:44 +0800

    #开发：添加element-icons库

--- src/main.js
+++ src/main.js
@@ -3,9 +3,13 @@ import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 const app = createApp(App)
 
 app.use(router)
 app.use(ElementPlus, { size: 'small', zIndex: 3000 }) // 全局配置
 app.mount('#app')
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
