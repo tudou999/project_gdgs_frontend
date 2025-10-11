b17f32fbcb34395f9c708d7f1d4e5bf162410708
Chen Zhuofan <148603759+tudou999@users.noreply.github.com>
2025-10-11 17:20:40 +0800

    #开发：引入pinia进行集中式数据管理

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
@@ -7,11 +8,15 @@ import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 // 引入暗黑主题
 import 'element-plus/theme-chalk/dark/css-vars.css'
 
+// 创建应用
 const app = createApp(App)
+const pinia = createPinia()
 
+// 使用路由器和Element Plus
 app.use(router)
 app.use(ElementPlus, { size: 'small', zIndex: 3000 }) // 全局配置
 app.mount('#app')
+app.use(pinia)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
