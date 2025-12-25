65514c7226220a652cb68f2a40a0436359eeb223
Chen Zhuofan <148603759+tudou999@users.noreply.github.com>
2025-12-25 12:55:52 +0800

    #优化：按需导入element + 手动分包（将elementui都打进一个chunk中，方便后续复用）

--- src/main.js
+++ src/main.js
@@ -2,13 +2,12 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
-import ElementPlus from "element-plus";
-import "element-plus/dist/index.css";
 import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 // 引入sentry
 import * as Sentry from "@sentry/vue";
 // 引入暗黑主题
 import "element-plus/theme-chalk/dark/css-vars.css";
+import "element-plus/es/components/message/style/css";
 
 // 创建应用
 const pinia = createPinia();
@@ -45,7 +44,6 @@ Sentry.init({
 
 // 使用路由器和Element Plus
 app.use(router);
-app.use(ElementPlus, { size: "small", zIndex: 3000 }); // 全局配置
 app.use(pinia);
 app.mount("#app");
 
